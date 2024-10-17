6dc5254016e159c5ba8ed6e983fa11862203d19c
paul-tes <paulasca8462@gmail.com>
2024-10-17 04:48:52 +0300

    Error

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         // Routes this applies to
-        source: "/api/(.*)",
+        source: "/api/(*)",
         // Headers
         headers: [
           // Allow for specific domains to have access or * for all
